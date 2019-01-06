572a57194cf8589f4c0b7db8a7a1865d60d470e1
Evan <evanchen333@gmail.com>
2019-01-06 20:16:34 +0900

    the Big Bang

---
+++ server/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, Blueprint, session, jsonify
+
+import sys
+
+app = Flask(__name__)
+app.config.update(
+    DEBUG=True,
+    SECRET_KEY=b'\x92d\xf2\xae\x03g\xb7\xa0O\xac[D\x13\x08\x1a\x94'
+)
+
+@app.route('/')
+def index():
+    return 'Home'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
